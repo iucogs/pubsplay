3568ef453c78bd6e81996ef39653be99d08f81e1
pjcraig <pjcraig@indiana.edu>
2013-10-28 13:00:13 -0400

    Some minor driver changes

--- app/models/Citations.scala
+++ app/models/Citations.scala
@@ -1,45 +1,45 @@
 package models
 
-import play.api.db.slick.Config.driver._
-import collection.mutable.Map
+//import play.api.db.slick.Config.driver._
+import scala.slick.driver.MySQLDriver.simple._
 
-//case class Citation(citation_id: Int, citation_metadata: Map[String, String], abs: String, doi: String, url: String, booktitle: String, chapter: Int, edition: String,
-//               translator: String, how_published: String, journal: String, month: Int, number: Int, pages: String, publisher: String, location: String, title: String,
-//               volume: String, year: String, date_retreived: String)
 
-case class Citation(citation_id: Int, doi: String, url: String)
-               
+
+case class Citation(citation_id: Int, pubtype: String, abs: String, keywords: String,      
+                    doi: String, url: String, booktitle: String, chapter: String, edition: String, editor:       
+                    String, translator: String, journal: String, month: String, number: String, pages: 
+                    String, publisher: String, location: String, title: String, volume: String, year: 
+                    String, raw: String, owner: String)
+
 object Citations extends Table[Citation]("citations") {
-  
   def citation_id = column[Int]("citation_id", O.PrimaryKey, O.AutoInc)
-  def citation_metadata = Map(("submitter", column[String]("submitter")),
-                              ("owner", column[String]("owner")),
-                              ("entry_time", column[Double]("entry_time")),
-                              ("raw", column[String]("raw")),
-                              ("keywords", column[String]("keywords")),
-                              ("annotation", column[String]("annotation")),
-                              ("pubtype", column[String]("pubtype")),
-                              ("filename", column[String]("filename")),
-                              ("verified", column[Int]("verified")))
+  def pubtype = column[String]("pubtype")
   def abs = column[String]("abstract")
+  def keywords = column[String]("keywords")
   def doi = column[String]("doi")
   def url = column[String]("url")
   def booktitle = column[String]("booktitle")
-  def chapter = column[Int]("chapter")
+  def chapter = column[String]("chapter")
   def edition = column[String]("edition")
+  def editor = column[String]("editor")
   def translator = column[String]("translator")
-  def how_published = column[String]("howpublished")
   def journal = column[String]("journal")
-  def month = column[Int]("month")
-  def number = column[Int]("number")
+  def month = column[String]("month")
+  def number = column[String]("number")
   def pages = column[String]("pages")
   def publisher = column[String]("publisher")
   def location = column[String]("location")
   def title = column[String]("title")
   def volume = column[String]("volume")
   def year = column[String]("year")
-  def date_retreived = column[String]("date_retreived")
-  def * = citation_id ~ doi ~ url <> (Citation, Citation.unapply _)
-  
-}
+  def raw = column[String]("raw")
+  def owner = column[String]("owner") 
 
+  def * = (citation_id ~ pubtype ~ abs ~ keywords ~ doi ~ url ~ booktitle ~ chapter ~   edition ~ editor ~ translator ~ journal ~ month ~ number ~ pages ~ publisher ~ location ~ title ~ volume ~ year ~ raw ~ owner).<>[Citation](Citation,Citation unapply _)
+
+  def add(citation: Citation)(implicit session: Session) = {
+    this.insert(citation)
+  } 
+  
+
+}
